2afea1d617afa177f89334edc8d4ef9b4266968d
Adnan Barwaniwala <107304756+adnanbarwaniwala@users.noreply.github.com>
2025-01-26 22:23:30 +0530

    Update 2_ChatDC.py

--- v3/pages/2_ChatDC.py
+++ v3/pages/2_ChatDC.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain_core.messages import HumanMessage, AIMessage
 from langchain.memory import ConversationBufferWindowMemory
-from v1_functions import *
+from functions import *
 
 st.set_page_config(
     page_title='ChatDC',
