2864bb76e55ac8b5fcb5cd54f5696a0b3a7ef2cf
Adnan Barwaniwala <107304756+adnanbarwaniwala@users.noreply.github.com>
2025-01-27 12:39:03 +0530

    Update 2_ChatDC.py

--- v3/pages/2_ChatDC.py
+++ v3/pages/2_ChatDC.py
@@ -22,6 +22,9 @@ with col2:
         unsafe_allow_html=True,
     )
 
+st.info("If the LLM isn’t able to answer your question, please try being more specific "
+        "and include all relevant information.", icon="ℹ️")
+
 if 'messages' not in st.session_state:
     st.session_state.messages = []
 if 'db' not in st.session_state:
